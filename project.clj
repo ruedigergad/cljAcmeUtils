4b9ef10f3895f4999709d80082e714e85ff9f80d
Ruediger Gad <r.c.g@gmx.de>
2012-11-13 21:04:58 +0100

    Update readme.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-;(defproject cljAcmeUtils "1.7.1"
-(defproject cljAcmeUtils "1.8.0-SNAPSHOT"
+(defproject clj-acme-utils "1.8.0"
+;(defproject clj-acme-utils "1.9.0-SNAPSHOT"
   :description "Unsorted bunch of helper and utility functions."
   :dependencies [[org.clojure/clojure "1.4.0"]]
 )
